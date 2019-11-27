1753d1871bfbb4cfc12440d93c4ebfcb6230ed86
Maximilian Gonzalez <thamaxlg@gmail.com>
2019-11-27 15:14:36 -0800

    okay, now stuff can be edited from the front end. that's pretty much the core functionality done. there are things that can probably be cleaned up or modularized more. also i should refactor some places (like editing for instance) so the screen doesn't change until it's guaranteed that the operation to the database went through properly. also i can eventually restructure this to support multiple users and allow them to share their data / progress

--- client/components/Note.jsx
+++ client/components/Note.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { updateNote, deleteNote } from '../actions/actions';
 
 const Note = ({note, _id}) => {
   const dispatch = useDispatch();
+  const [editing, setEditing] = useState(false);
+  const editRef = useRef(null);
 
+  // maybe find a way to only allow one edit to be activated at once? (sounds like a job for redux but we'll get to it)
   return (
     <li>
       {note}
       <br/>
-      <button>edit</button>
+      <button onClick={() => setEditing(true)}>edit</button>
       <button onClick={() => dispatch(deleteNote(_id, note))}>delete</button>
+      {editing && 
+        <div className="container">
+          <textarea ref={editRef}>{note}</textarea>
+          <button onClick={() => {
+            if (editRef.current.value !== '') {
+              dispatch(updateNote(_id, note, editRef.current.value));
+              editRef.current.value = '';
+              setEditing(false);
+            }
+          }}>save</button>
+          <button onClick={() => setEditing(false)}>cancel</button>
+        </div>
+      }
     </li>
   );
 };
